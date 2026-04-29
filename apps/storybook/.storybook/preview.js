d053540c1d0e8e621c03fe9234a306f4509cf2bc
Ramon Xavier <ramon1234rxm@gmail.com>
2026-04-29 16:31:54 -0300

    feat: initialize storybook app with base configuration and stories

---
+++ apps/storybook/.storybook/preview.js
@@ -0,0 +1,15 @@
+import '../input.css';
+
+/** @type { import('@storybook/html-vite').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
